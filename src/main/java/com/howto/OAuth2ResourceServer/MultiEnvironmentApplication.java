8550b9b7b23fbff6c44b1ff19a3900f8985ac2fc
osparking <jbpark03@gmail.com>
2023-07-17 23:12:00 +0900

    project initialized with 2 dependencies

---
+++ src/main/java/com/howto/OAuth2ResourceServer/MultiEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.howto.OAuth2ResourceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiEnvironmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/howto/OAuth2ResourceServer/MultiEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howto.OAuth2ResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
