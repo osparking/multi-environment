f88c633c95159d7cad486f9c207f403ffe4e90bf
osparking <jbpark03@gmail.com>
2023-07-18 10:03:10 +0900

    package renamed to match tutorial

--- src/main/java/com/lemoncode21/multienvironment/controller/MultiEnvironmentApplication.java
+++ src/main/java/com/lemoncode21/multienvironment/controller/MultiEnvironmentApplication.java
@@ -1,4 +1,4 @@
-package com.howto.OAuth2ResourceServer;
+package com.lemoncode21.multienvironment.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
