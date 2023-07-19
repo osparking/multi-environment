c61a7cea7d5c37f47cec2951a42e583619416006
osparking <jbpark03@gmail.com>
2023-07-19 23:51:13 +0900

    Unable to find a @SpringBootConfiguration error fix

--- src/test/java/com/howto/OAuth2ResourceServer/MultiEnvironmentApplicationTests.java
+++ src/test/java/com/howto/OAuth2ResourceServer/MultiEnvironmentApplicationTests.java
@@ -3,7 +3,9 @@ package com.howto.OAuth2ResourceServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.lemoncode21.multienvironment.controller.MultiEnvironmentApplication;
+
+@SpringBootTest(classes = MultiEnvironmentApplication.class)
 class MultiEnvironmentApplicationTests {
 
 	@Test
