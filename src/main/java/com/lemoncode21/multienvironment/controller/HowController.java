59b1e94b88ac726f08a1c6f60a5560343e1f0307
osparking <jbpark03@gmail.com>
2023-07-20 09:34:17 +0900

    api method in restcontroller

--- src/main/java/com/lemoncode21/multienvironment/controller/HowController.java
+++ src/main/java/com/lemoncode21/multienvironment/controller/HowController.java
@@ -1,5 +1,17 @@
 package com.lemoncode21.multienvironment.controller;
 
-public class HowController {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HowController {
+	
+	@Value(value = "${spring.profiles.active}")
+	private String profile;
+
+	@GetMapping("/")
+	public String homepage() {
+		return "지금 활동 중인 프로필: " + profile;
+	}
 }
